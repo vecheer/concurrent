03dfdc5262f67ae1250d4865b9865a0b1bb035e1
vecheer <1571462409@qq.com>
2022-06-07 22:02:46 +0800

    类锁和对象锁

--- CH4-sharing-model/src/main/java/L2_synchronized/X_synchronized_question.java
+++ CH4-sharing-model/src/main/java/L2_synchronized/X_synchronized_question.java
@@ -2,51 +2,45 @@ package L2_synchronized;
 
 import lombok.Data;
 
-@Data
-class LockHolder {
-    private int count = 0;
 
-    public void increase() {
-        synchronized (this) {
+@Data
+class CountHolder {
+    private static int count = 0;
+
+    public  void increase() {
+        synchronized (this){
             count++;
         }
     }
 
-    public void decrease() {
-        synchronized (this) {
+    public  static void decrease() {
+        synchronized (CountHolder.class){
             count--;
         }
     }
 
-    public int getCount() {
-        synchronized (this) {
-            return count;
-        }
+    public static int getCount() {
+        return count;
     }
 }
 
-/**
- * @author yq
- * @version 1.0
- * @date 2022/6/6 0:10
- */
-public class C2_synchronized_object_plus {
+public class X_synchronized_question {
 
     static final CountHolder holder = new CountHolder();
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 5000; i++) {
+            for (int i = 0; i < 500000; i++) {
                 holder.increase();
             }
-        });
+        },"t1");
 
 
         Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 5000; i++) {
+            for (int i = 0; i < 500000; i++) {
                 holder.decrease();
             }
-        });
+        },"t2");
 
         t1.start();
         t2.start();
@@ -54,6 +48,6 @@ public class C2_synchronized_object_plus {
         t1.join();
         t2.join();
 
-        System.out.println(holder.getCount());
+        System.out.println(CountHolder.getCount());
     }
 }
