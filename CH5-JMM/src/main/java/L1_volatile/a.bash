1207849516ed7de274daa2e27f51484aa988d636
vecheer <1571462409@.com>
2022-06-30 23:45:42 +0800

    原子引用 AtomicReference 自定义一个原子Integer

---
+++ CH5-JMM/src/main/java/L1_volatile/a.bash
@@ -0,0 +1,6 @@
+
+
+new           #6    // new Singleton()   新建对象
+dup
+invokespecial #7    // Singleton.<init>  执行构造器
+putstatic     #2    // put field         赋值给静态字段 INSTANCE
--- CH6-no-lock/src/main/java/L2_Atomic/C2_AtomicReference.java
+++ CH6-no-lock/src/main/java/L2_Atomic/C2_AtomicReference.java
@@ -1,21 +1,64 @@
 package L2_Atomic;
 
+import org.springframework.util.StopWatch;
+
 import java.util.concurrent.atomic.AtomicReference;
 
 public class C2_AtomicReference {
 
-    public static void main(String[] args) {
-        AtomicReference<MyNum> myNum = new AtomicReference<>();
+    static MySafeInteger num = new MySafeInteger(0);
 
+    public static void main(String[] args) throws InterruptedException {
+        StopWatch watch = new StopWatch();
+        watch.start();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                num.add(5);
+            }
+        });
+
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                num.add(-5);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        watch.stop();
+        System.out.println("结果: " + num.getValue() + "  耗时: " + watch.getLastTaskTimeMillis());
 
     }
 
+    // 自定义一个 线程安全的int
+    static class MySafeInteger {
 
-    static class MyNum{
-        private int value;
+        private AtomicReference<Integer> numReference;
 
-        public void add(int d){
-            value += d;
+        public MySafeInteger(Integer num) {
+            this.numReference = new AtomicReference<>(num);
+        }
+
+        // CAS 实现
+        public void add(int d) {
+            Integer E, N;
+            do {
+                E = numReference.get();
+                N = E + d;
+            } while (!numReference.compareAndSet(E, N));
+        }
+
+        // 获取值
+        public int getValue() {
+            return numReference.get() == null ? 0 : numReference.get();
         }
     }
+
 }
+
